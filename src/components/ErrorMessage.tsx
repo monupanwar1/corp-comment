fcd6143bf1b4a4d5c9d3cc24afe932dd1a7c4608
panwarkunal <monupanwar5655@gmail.com>
2025-01-27 21:35:52 +0530

    short-circuit

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,3 +1,7 @@
-export default function ErrorMessage({ message }) {
+ interface ErrorMessageprops{
+    message:React.ReactNode;
+} 
+
+export default function ErrorMessage({ message }:ErrorMessageprops) {
     return <div>{message}</div>;
   }
--- src/components/FeedbackLIst.tsx
+++ src/components/FeedbackLIst.tsx
@@ -1,49 +1,49 @@
 import { useEffect, useState } from "react";
 import FeedbackItems from "./FeedbackItems";
-import { TFeedbackItem } from "./lib/type";
+// import { TFeedbackItem } from "./lib/type";
 import Spinner from "./Spinner";
 import ErrorMessage from "./ErrorMessage";
 
 
 // dummy data 
-const examplefeedbackItems:TFeedbackItem[]=[
-  {
-  id: 1,
-  upvoteCount: 3,
-  badgeLetter:"N",
-  company: "netflix",
-  text: "nextflix is the best app to watch movie",
-  daysAgo: 10,
+// const examplefeedbackItems:TFeedbackItem[]=[
+//   {
+//   id: 1,
+//   upvoteCount: 3,
+//   badgeLetter:"N",
+//   company: "netflix",
+//   text: "nextflix is the best app to watch movie",
+//   daysAgo: 10,
 
-  },
-  {
-    id: 2,
-    upvoteCount: 4,
-    badgeLetter:" S",
-    company: "spotify",
-    text: "spotify is the best app to listen movie",
-    daysAgo: 11,
+//   },
+//   {
+//     id: 2,
+//     upvoteCount: 4,
+//     badgeLetter:" S",
+//     company: "spotify",
+//     text: "spotify is the best app to listen movie",
+//     daysAgo: 11,
   
-    },
-  {
-    id: 3,
-    upvoteCount: 4,
-    badgeLetter:" P",
-    company: "spotify",
-    text: "spotify is the best app to listen movie",
-    daysAgo: 11,
+//     },
+//   {
+//     id: 3,
+//     upvoteCount: 4,
+//     badgeLetter:" P",
+//     company: "spotify",
+//     text: "spotify is the best app to listen movie",
+//     daysAgo: 11,
   
-    },
-  {
-    id: 2,
-    upvoteCount: 6,
-    badgeLetter:" l",
-    company: "spotify",
-    text: "spotify is the best app to listen movie",
-    daysAgo: 11,
+//     },
+//   {
+//     id: 2,
+//     upvoteCount: 6,
+//     badgeLetter:" l",
+//     company: "spotify",
+//     text: "spotify is the best app to listen movie",
+//     daysAgo: 11,
   
-    }
-]
+//     }
+// ]
 export default function FeedbackLIst() {
   const [feedbackItems,setFeedbackItems]=useState([]);// for data 
   const [isLoading,setIsLoading]=useState(false);// for loader
@@ -60,6 +60,7 @@ export default function FeedbackLIst() {
       }
       const data = await response.json();
       setFeedbackItems(data.feedbacks)
+      setIsLoading(false);
 
     }catch(e){
       setErrorMessage("Something went wrong ,please try later");
@@ -89,14 +90,19 @@ export default function FeedbackLIst() {
   // },[])
   return (
     <ol className="feedback-list">
-      {
-        isLoading?<Spinner/>:null
-      }
+
+       {/* show loading spinner if data is loading */}
+       {isLoading && <Spinner/>}
+
+       {/* show error message if any error occurs  by using terinary*/}
+      {/* { isLoading?<Spinner/>:null}         */}
+
+     
       {
         errorMessage?<ErrorMessage message={errorMessage}/> :null
       }
       {feedbackItems.map((item)=>{
-        return <FeedbackItems key={item.id} feebackItems={item}/>
+        return <FeedbackItems  feebackItems={item}/>
       })}
        
     </ol>
