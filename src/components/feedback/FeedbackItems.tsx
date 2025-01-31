c327db1077b407bd0e25757a8054e18f844b0c01
panwarkunal <monupanwar5655@gmail.com>
2025-01-31 09:12:42 +0530

    upvote funcnality added

--- src/components/feedback/FeedbackItems.tsx
+++ src/components/feedback/FeedbackItems.tsx
@@ -1,16 +1,30 @@
 import { TriangleUpIcon } from "@radix-ui/react-icons";
 import { TFeedbackItem } from "../lib/type";
+import { useState } from "react";
 
 interface FeedbackItemsProps{
   feebackItems:TFeedbackItem;
 }
 
 export default function FeedbackItems({feebackItems}:FeedbackItemsProps) {
+ const [open, setOpen] = useState(false);
+ const[upvoteCount,setUpvoteCount]=useState(feebackItems.upvoteCount);
+
+ const handleUpvote =( e:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
+  setUpvoteCount((prev)=>prev+1);
+  e.currentTarget.disabled =true;
+  e.stopPropagation();
+
+ }
+
   return (
-    <li className="feedback">
-            <button>
+    <li 
+    onClick={()=>setOpen((open)=>!open)}
+    className={`feedback ${open ?"feedback--expand" : ""}`}
+    >
+            <button onClick={handleUpvote}>
                 <TriangleUpIcon/>
-                <span>{feebackItems.upvoteCount}</span>
+                <span>{upvoteCount}</span>
             </button>
             <div>{feebackItems.badgeLetter}</div>
             <div>
